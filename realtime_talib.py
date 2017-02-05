b6e480007500ce543e08cce5ea791576f01b7f91
Michael Usachenko <musachenko@gmail.com>
2017-02-05 14:03:57 -0600

    rtt.py cleanup/optimization
    Cleaned up realtime_talib, test.py is updated, readme is next

--- realtime_talib.py
+++ realtime_talib.py
@@ -1,54 +1,53 @@
 import parse_data as pd
 import numpy as np
 import talib
-import time 
 
 from talib import abstract
 from talib.abstract import *
 
-def realtime_init (ticker, start_date, end_date):
+def realtime_init (exchange, ticker, start_date, end_date, inc_delayed_volume):
+
 	nump_hist_data = np.asarray(pd.pull_historical_data(ticker, start_date, end_date, True), dtype=float)
 
 	inputs = {
-  		'open': nump_hist_data[0],
-    	'high': nump_hist_data[1],
-    	'low': nump_hist_data[2],
-    	'close': nump_hist_data[3],
-    	'volume': nump_hist_data[4]
+  		'open': nump_hist_data[1],
+    	'high': nump_hist_data[2],
+    	'low': nump_hist_data[3],
+    	'close': nump_hist_data[4],
+    	'volume': nump_hist_data[5]
 	}
 
-	return inputs
+	return [inputs, exchange, ticker, inc_delayed_volume]
 
 
-def realtime_SMA(exchange, ticker, start_date, end_date, timeperiod, inc_delayed_volume):
+def realtime_MA(ma_type, timeperiod, init):
 
-	inputs = realtime_init(ticker,start_date,end_date) 
-	sma = abstract.SMA
-	output = SMA(inputs, timeperiod)
-	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(exchange,ticker,inc_delayed_volume)[1]
+	inputs = init[0]
+	ma = abstract.MA
+	output = MA(inputs, timeperiod, ma_type)
+	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(init[1],init[2],init[3])[1]
 	return output[len(output)-1]
 
-def realtime_EMA(exchange, ticker, start_date, end_date, timeperiod, inc_delayed_volume):
+def realtime_MACD(ma_type, fastperiod, slowperiod, signalperiod, init):
 
-	inputs = realtime_init(ticker,start_date,end_date) 
-	ema = abstract.EMA
-	output = EMA(inputs, timeperiod)
-	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(exchange,ticker,inc_delayed_volume)[1]
+	inputs = init[0]
+	macdreg = abstract.MACD
+	macd, macdsignal, macdhist = MACD(inputs, fastperiod, slowperiod, signalperiod)
+	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(init[1],init[2],init[3])[1]
+	return [macd[len(macd)-1],macdsignal[len(macdsignal)-1],macdhist[len(macdhist)-1]]
+
+def realtime_RSI(ma_type, timeperiod, init):
+
+	inputs = init[0]
+	rsi = abstract.RSI
+	output = RSI(inputs, timeperiod, ma_type)
+	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(init[1],init[2],init[3])[1]
 	return output[len(output)-1]	
 
-def realtime_DEMA(exchange, ticker, start_date, end_date, timeperiod, inc_delayed_volume):
+def realtime_BBANDS(ma_type, nbdevup, nbdevdn, timeperiod, init):
 
-	inputs = realtime_init(ticker,start_date,end_date) 
-	dema = abstract.DEMA
-	output = DEMA(inputs, timeperiod)
-	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(exchange,ticker,inc_delayed_volume)[1]
-	return output[len(output)-1]	
-
-def realtime_TEMA(exchange, ticker, start_date, end_date, timeperiod, inc_delayed_volume):
-
-	inputs = realtime_init(ticker,start_date,end_date) 
-	tema = abstract.TEMA
-	output = TEMA(inputs, timeperiod)
-	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(exchange,ticker,inc_delayed_volume)[1]
-	return output[len(output)-1]	
-
+	inputs = init[0]
+	bbands = abstract.BBANDS
+	upperband, middleband, lowerband = BBANDS(inputs, timeperiod, nbdevup, nbdevdn, ma_type)
+	inputs['close'][len(inputs['close'])-1] = pd.pull_live_data(init[1],init[2],init[3])[1]
+	return [upperband[len(upperband)-1],middleband[len(middleband)-1],lowerband[len(lowerband)-1]]
--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 import realtime_talib as rtt
 import time
 
+# nump_live_data = np.asarray(rtt.pull_live_data('NASDAQ','SPY', False))
+# nump_hist_data= np.asarray(rtt.pull_historical_data('NVDA','2017-01-28','2017-02-01'))
 
-nump_live_data = np.asarray(rtt.pull_live_data('NASDAQ','SPY', False))
-nump_hist_data= np.asarray(rtt.pull_historical_data('NVDA','2017-01-28','2017-02-01'))
-
-print nump_live_data[1]
-print nump_hist_data[0][0]
-print rtt.formatted_price('NYSE','AAPL',5)
-
+# print nump_live_data[1]
+# print nump_hist_data[0][0]
+# print rtt.formatted_price('NYSE','AAPL',5)
 
 while True:
 
-	print rtt.realtime_SMA('NYSE','SPY','2017-01-23','2017-02-04',2,False)
+	init = rtt.realtime_init('NASDAQ','SPY','2017-01-28','2017-02-01',False)
+	
+	print rtt.realtime_MA(1, 3, init)
+	print rtt.realtime_MACD(1, 12, 26, 9, init)[2]
+	print rtt.realtime_BBANDS(1, 2, 2, 10, init)[1]
+	print rtt.realtime_RSI(1, 25, init)
+
 	time.sleep(1)
