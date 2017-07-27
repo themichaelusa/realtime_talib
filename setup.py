f8e8b70ca55780df4e754d12ad148795a915993d
Michael Usachenko <senseikawasaki@gmail.com>
2017-07-26 22:28:39 -0400

    added pypi creds

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'realtime_talib',
+  packages = ['realtime_talib'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Generate Realtime technical indicators for algotrading/fun',
+  author = 'Michael Usachenko',
+  author_email = 'meu2@illinois.edu',
+  url = 'https://github.com/themichaelusa/realtime_talib', # use the URL to the github repo
+  download_url = 'https://github.com/themichaelusa/realtime_talib/archive/0.2.tar.gz', # I'll explain this in a second
+  keywords = ['realtime', 'talib'], # arbitrary keywords
+  classifiers = [],
+)
